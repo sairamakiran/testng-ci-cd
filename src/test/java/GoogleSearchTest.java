2bb47326b39e0e15a348d3de02cb572580c9e661
sairamakiran <sairamnadh230@gmail.com>
2025-07-21 12:33:37 +0530

    new changes

---
+++ src/test/java/GoogleSearchTest.java
@@ -0,0 +1,46 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class GoogleSearchTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        // Set Chrome options to run in incognito
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("--start-maximized");
+
+        driver = new ChromeDriver(options);
+    }
+
+    @Test(groups = {"sanity", "regression"})
+    public void testGoogleSearch() {
+        driver.get("https://www.google.com");
+
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Carelon Global Solutions");
+        searchBox.submit();
+
+        // Wait and validate result
+        WebElement resultStats = driver.findElement(By.id("result-stats"));
+        Assert.assertTrue(resultStats.isDisplayed(), "Search results not displayed");
+    }
+
+    @AfterMethod
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
